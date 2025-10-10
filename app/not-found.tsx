ba73f5fe7be3bcb18fcfb06f4510a0763254e0c4
Lakshya-coder04 <Lakshyabirla7cr@gmail.com>
2025-10-10 17:25:16 -0600

    Create not-found.tsx

---
+++ app/not-found.tsx
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <div className='flex flex-col items-center justify-center min-h-screen'>
+      <h1 className='text-3xl font-bold mb-4'>404 – Page not found</h1>
+      <p className='text-gray-500'>Sorry, we couldn’t find that page.</p>
+    </div>
+  );
+}
