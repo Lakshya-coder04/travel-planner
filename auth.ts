d9540ef4c1ae8d37e2fad38d8ad155c252cf1fca
Lakshya-coder04 <Lakshyabirla7cr@gmail.com>
2025-10-10 18:40:41 -0600

    Update auth.ts

--- auth.ts
+++ auth.ts
@@ -1,9 +1,12 @@
 import NextAuth from 'next-auth';
-import Github from 'next-auth/providers/github';
 import { PrismaAdapter } from '@auth/prisma-adapter';
 import { prisma } from './lib/prisma';
+import GitHub from 'next-auth/providers/github';
 
 export const { auth, handlers, signIn, signOut } = NextAuth({
-  providers: [Github],
+  providers: [GitHub],
   adapter: PrismaAdapter(prisma), //it's only necessary only if you are using an orm like Prisma which is going to adapt and store authenticated user info into the database
+  session: {
+    strategy: 'jwt', // or 'database' if you prefer
+  },
 });
