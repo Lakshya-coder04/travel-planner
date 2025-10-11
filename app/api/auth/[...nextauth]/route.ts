86507bbb847bdc2d152926ddbbe2824b452f36fb
Lakshya-coder04 <Lakshyabirla7cr@gmail.com>
2025-10-10 18:26:40 -0600

    .

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,8 @@
-export const dynamic = 'force-dynamic';
-export const runtime = 'nodejs';
-
-import { handlers } from '../../../..//auth';
+import { handlers } from '@/auth';
 
 export const { GET, POST } = handlers;
+
+// Force Next.js to not statically analyze this route
+export const dynamic = 'force-dynamic';
+export const runtime = 'nodejs';
+export const preferredRegion = 'auto';
